bb1ba36fb6b0d763f6efa9c349a2acdc7ec73cdb
Eric Furrevig <efurrevig@gmail.com>
2023-04-23 20:04:22 -0400

    Added eslint

--- index.js
+++ index.js
@@ -24,18 +24,19 @@ app.get('/api/persons', (request, response) => {
 
 //get individual persons
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id).then(person => {
-        if (person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
-    })
-    .catch(error => next(error))
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 //delete persons
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
         .then(result => {
             response.status(204).end()
@@ -71,10 +72,10 @@ app.put('/api/persons/:id', (request, response, next) => {
     const { name, number } = request.body
 
     Person.findByIdAndUpdate(
-        request.params.id, 
-        { name, number }, 
-        {new: true, runValidators: true, context: 'query'}
-        )
+        request.params.id,
+        { name, number },
+        { new: true, runValidators: true, context: 'query' }
+    )
         .then(newPerson => {
             response.json(newPerson)
         })
@@ -89,16 +90,16 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-  
+
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
     }
-  
+
     next(error)
-  }
-  
+}
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT
